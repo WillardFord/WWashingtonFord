0b8cd1227876da025ebef795672594b7377c36d5
Willard Washington Ford <53413671+WWashingtonFord@users.noreply.github.com>
2022-09-22 16:15:02 -0700

    Update log_normalize.py

--- log_normalize.py
+++ log_normalize.py
@@ -4,9 +4,12 @@ import sys
 
 ## Log Normalize:
 
-input = pd.read_csv(sys.argv[1], delimiter="\t")
+input = pd.read_csv(sys.argv[1], delimiter="\t", skiprows=2)
+
 ## To normalize by row swap rows and columns
-inputT = input.T
+inp_no_desc = input.drop(['Name','Description'], axis = 1)
+inputT = inp_no_desc.T
+
 for column in inputT.columns:
     # eliminate negative values by column
     minValue = inputT[column].min()
@@ -14,15 +17,20 @@ for column in inputT.columns:
         inputT[column] += abs(minValue)
     # log normalize
     inputT[column] = np.log(inputT[column])
+output = inputT.T
 
-input = inputT.T
+# Add back in non-numeric columns
+columns = input.columns
+for i in range(2):
+    output.insert(i, columns[i], input[columns[i]])
+
 out_filename = "output.gct"
+output.to_csv("data_df", sep = "\t", index= False)
 
-transformedData = input.to_csv("data_df", sep = "\t")
-with open(transformedData, 'r'):
+with open("data_df", 'r') as transformedData:
     data = transformedData.read()
 with open(out_filename, 'w') as outFile:
-    outFile.write(f"#1.2\n{input.shape[0]}\t{input.shape[1]}")
+    outFile.write(f"#1.2\n{input.shape[0]}\t{input.shape[1]-2}\n")
 with open(out_filename, 'a') as outFile:
     outFile.write(data)
 
